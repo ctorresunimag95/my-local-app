e3b4dee37c84759727f6cb8ef6348dbe6453d9aa
Camilo Torres <ctorresunimag95@gmail.com>
2025-03-30 18:22:41 -0500

    Use withreference method

--- Cinema.AppHost/Program.cs
+++ Cinema.AppHost/Program.cs
@@ -23,15 +23,14 @@ var cosmos = builder.AddAzureCosmosDB("cosmos-db").RunAsEmulator(
 
 
 var management = builder.AddProject<Projects.Cinema_Management>("management")
-    .WithEnvironment("ConnectionStrings__service-bus", serviceBus.Resource.ConnectionStringExpression)
-    .WithEnvironment("ConnectionStrings__cosmos-db",
-        cosmos.Resource.ConnectionStringExpression)
+    .WithReference(serviceBus, connectionName: "service-bus")
+    .WithReference(cosmos, connectionName: "cosmos-db")
     .WaitFor(serviceBus)
     .WaitFor(cosmos);
 
 var reservation = builder.AddProject<Projects.Cinema_Reservation>("reservation")
-    .WithEnvironment("ConnectionStrings__redis", cache.Resource.ConnectionStringExpression)
-    .WithEnvironment("ConnectionStrings__service-bus", serviceBus.Resource.ConnectionStringExpression)
+    .WithReference(cache, "redis")
+    .WithReference(serviceBus, "service-bus")
     .WaitFor(cache)
     .WaitFor(serviceBus)
     // Added this wait to avoid service bus connectivity error. Waiting for management will help to wait SB being ready
